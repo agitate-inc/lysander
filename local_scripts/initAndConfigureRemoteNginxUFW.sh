2c6a2b1642c105000560c610e37f12c5bd309ad1
B <brandonburke.personal@gmail.com>
2019-07-24 16:16:00 -0500

    Perfected makeManager functionality.

--- local_scripts/initAndConfigureRemoteNginxUFW.sh
+++ local_scripts/initAndConfigureRemoteNginxUFW.sh
@@ -2,12 +2,14 @@ remoteIP=$1
 siteName=$2
 ##The remote.nginx_x.conf files must be prepared before this script is launched.
 
-scp -i ~/.ssh/rmroot_rsa ~/lysander/resources/scp_scripts/installNginxAndSetupUFW.scp root@"$remoteIP":/root;
+scp -i ~/.ssh/rmroot_rsa ~/lysander/resources/scp_scripts/installNginxAndSetupUFW.scp root@"$remoteIP":/root/installNginxAndSetupUFW.sh;
 ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "bash ~/installNginxAndSetupUFW.sh";
 scp -i ~/.ssh/rmroot_rsa ~/lysander/resources/remote.nginx_precert.scp root@"$remoteIP":/etc/nginx/nginx.conf;
 ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "yes | service nginx restart";
 ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "sudo certbot certonly -d "$siteName" -q --nginx --keep-until-expiring --register-unsafely-without-email --agree-tos"
-ssh -i ~/.ssh/rm_root_rsa root@"$remoteIP" "chgrp -R letsencrypt /etc/letsencrypt chmod -R g=rX /etc/letsencrypt"
+ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "sudo certbot certonly -d "$siteName" -q --nginx --keep-until-expiring --register-unsafely-without-email --agree-tos"
+ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "groupadd -g 1024 letsencrypt"
+ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "chgrp -R letsencrypt /etc/letsencrypt; chmod -R g=rX /etc/letsencrypt"
 scp -i ~/.ssh/rmroot_rsa ~/lysander/resources/remote.nginx_postcert.scp root@"$remoteIP":/etc/nginx/nginx.conf;
 ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "yes | service nginx restart";
 ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "sudo certbot certonly -d flawlesscougarbuffet.tk -q --nginx --keep-until-expiring --register-unsafely-without-email --agree-tos"
--- local_scripts/makeAndSecureRemoteManager.sh
+++ local_scripts/makeAndSecureRemoteManager.sh
@@ -1,13 +1,14 @@
 remoteIP=$1
 
-scp -i ~/.ssh/rmroot_rsa ~/lysander/resources/scp_scripts/makeManager.scp root@"$remoteIP":/root;
-ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "bash ~/makeManager.scp";
+scp -i ~/.ssh/rmroot_rsa ~/lysander/resources/scp_scripts/makeManager.scp root@"$remoteIP":/root/makeManager.sh;
+ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "bash ~/makeManager.sh";
 
 ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "mkdir -p /home/manager/.ssh; touch /home/manager/.ssh/authorized_keys; \
 chown -r /home/manager manager:manager; chmod -R 600 /home/manager";
 
-ssh-keygen -t rsa -f ~/.ssh/rmmanager_rsa -N '';
-scp -i ~/.ssh/rmroot_rsa ~/.ssh/rmmanager_rsa.pub root@"$remoteIP":/root;
+yes | ssh-keygen -t rsa -f ~/.ssh/rmmanager_rsa -N '';
+scp -i ~/.ssh/rmroot_rsa ~/.ssh/rmmanager_rsa.pub root@"$remoteIP":/root/rmmanager_rsa.pub;
+
 ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "cat ~/rmmanager_rsa.pub >> /home/manager/.ssh/authorized_keys";
 
 ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "sed -i '32s/.*/PermitRootLogin no/' /etc/ssh/sshd_config; service sshd restart";
