6c8d94088f227bbb8d222fe189a5a00b745a84c7
B <brandonburke.personal@gmail.com>
2019-07-24 12:01:43 -0500

    Perfected automated secureRemoteRoot.sh execution when provided with correct information.

--- local_scripts/secureRemoteRoot.sh
+++ local_scripts/secureRemoteRoot.sh
@@ -10,7 +10,7 @@ remoteRootPassword=$3
 remoteKeyUntrusted="$(ssh-keyscan $remoteIP -t rsa 2>/dev/null)"
 remoteFingerprintUntrusted="$(echo $remoteKeyUntrusted | ssh-keygen -lf - | awk '{print $2}')"
 
- echo "$remoteRootPassword" >> ~/.ssh/remoteRootPassword.txt;
+ echo "$remoteRootPassword" > ~/.ssh/remoteRootPassword.txt;
 #Note : The remote server must have one and only one RSA fingerprint. Otherwise the fingerprint-verification op will fail.
 
 #If the fingerprint of the retrieved remote key is equal to the fingerprint provided by the user, then add to known hosts.
@@ -22,7 +22,7 @@ if [ "$remoteFingerprintTrusted" != "$remoteFingerprintUntrusted" ]
 remoteKeyTrusted="$remoteKeyUntrusted"
 echo "$remoteKeyTrusted" >> ~/.ssh/known_hosts
 
-ssh-keygen -t rsa -f ~/.ssh/rmroot_rsa -N ''
+yes | ssh-keygen -t rsa -f ~/.ssh/rmroot_rsa -N ''
 sshpass -f ~/.ssh/remoteRootPassword.txt ssh-copy-id -i ~/.ssh/rmroot_rsa.pub root@"$remoteIP"
 ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "sed -i '61s/.*/ChallengeResponseAuthentication no/' \
 /etc/ssh/sshd_config; sed -i '56s/.*/PasswordAuthentication no/' /etc/ssh/sshd_config; \
