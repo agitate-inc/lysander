6b4b92704f47e32beaed139e5771d8a3c870b549
B <brandonburke.personal@gmail.com>
2019-07-24 11:44:44 -0500

    Added scripts to automate certificate acquisition and properly configure remote Nginx server thereafter.

---
+++ local_scripts/makeAndSecureRemoteManager.sh
@@ -0,0 +1,13 @@
+remoteIP=$1
+
+scp -i ~/.ssh/rmroot_rsa ~/lysander/resources/scp_scripts/makeManager.scp root@"$remoteIP":/root;
+ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "bash ~/makeManager.scp";
+
+ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "mkdir -p /home/manager/.ssh; touch /home/manager/.ssh/authorized_keys; \
+chown -r /home/manager manager:manager; chmod -R 600 /home/manager";
+
+ssh-keygen -t rsa -f ~/.ssh/rmmanager_rsa -N '';
+scp -i ~/.ssh/rmroot_rsa ~/.ssh/rmmanager_rsa.pub root@"$remoteIP":/root;
+ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "cat ~/rmmanager_rsa.pub >> /home/manager/.ssh/authorized_keys";
+
+ssh -i ~/.ssh/rmroot_rsa root@"$remoteIP" "sed -i '32s/.*/PermitRootLogin no/' /etc/ssh/sshd_config; service sshd restart";
