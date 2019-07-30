0592ce9eb7117d9659ef0adbf141effe953f4c7a
B <brandonburke.personal@gmail.com>
2019-07-30 09:24:53 -0500

    Made changes to local UFW setup for better functionality.

--- local_scripts_am/initAndConfigureLocalNginxUFW.sh
+++ local_scripts_am/initAndConfigureLocalNginxUFW.sh
@@ -1,5 +1,7 @@
 sudo apt -y install nginx
 sudo apt -y install ufw
 cp ~/lysander/resources/local.nginx.conf /etc/nginx/nginx.conf
-yes | ufw allow 'Nginx HTTPS'
+ufw allow 'Nginx HTTPS'
+ufw allow 'ssh'
+yes | ufw reload
 sudo service nginx restart
